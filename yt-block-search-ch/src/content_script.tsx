0a479068d9e7b22bff7d76c9cfc6c040faf5546c
1k-ct <59008019+1k-ct@users.noreply.github.com>
2022-01-01 04:22:13 +0900

    update import, del commentout

--- yt-block-search-ch/src/content_script.tsx
+++ yt-block-search-ch/src/content_script.tsx
@@ -1,21 +1,15 @@
-import { URLs } from "./blockChannel";
+import iURLs from "./data/channel.json";
 
-interface channelHTMLElement extends HTMLElement {
-  href: string | null;
-}
+const URLs = iURLs.URL;
+
 const hideChannelContent = (channelURL: string) => {
-  // document.querySelector("#channel-info > a").parentElement.__dataHost.style.display = 'none';
-  // document.querySelectorAll("#channel-info > a").forEach((data)=>console.log(data.href))
   const content = "#channel-info > a";
   const contentSection = document.querySelectorAll<HTMLLinkElement>(content);
 
   for (const iSection of contentSection) {
-    // if (iSection.__dataHost == null) { return; }
     if (iSection.href == channelURL) {
-      // breakは間違い多分
-      // iSection.__dataHost.style.display = 'none';
-      // const item = iSection.closest<HTMLElement>("ytd-video-renderer");
-      // const item = iSection.parentElement.parentElement.parentElement.parentElement
+      // TODO breakは間違い多分
+      // とりあえずこのままで
       const item =
         iSection.parentElement?.parentElement?.parentElement?.parentElement ??
         null;
@@ -27,31 +21,11 @@ const hideChannelContent = (channelURL: string) => {
   }
 };
 
-
 window.addEventListener("load", async () => {
-  // let defaultUserPreference: userPreference = {
-  //   enable: true,
-  //   // forbiddenWords: [],
-  //   forbiddenChannels: [],
-  // };
-  // const key = "user_preference_json_key";
-  // let userPreference =
-  //   (await loadUserPreference(key)) ?? defaultUserPreference;
-
-  // const testUserPreference: userPreference = {
-  //   enable: true,
-  //   forbiddenChannels: urls
-  // }
-  // await saveUserPreference(key, testUserPreference)
-
-  // for (const url of userPreference.forbiddenChannels) {
-  //   hideChannelContent(url);
-  // }
   for (const url of URLs) {
     hideChannelContent(url);
   }
 });
-// #contents > ytd-video-renderer:nth-child(1)
 
 window.addEventListener("scroll", async () => {
   for (const url of URLs) {
